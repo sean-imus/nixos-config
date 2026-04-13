d2bcf51769eccedc75b85c2d0ba5cacd973c7007
Sean Tietz <sean.tietz2@gmail.com>
2026-04-13 11:48:47 +0200

    moved to unstable

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
 
   inputs = {
     # Package Source
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-unstable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
