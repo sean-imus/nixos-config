e8f8c8a85c412ce0052692e4fd01bd58627108af
Sean Tietz <sean.tietz2@gmail.com>
2026-04-16 14:39:13 +0200

    smaller font

--- dotfiles/alacritty.nix
+++ dotfiles/alacritty.nix
@@ -6,7 +6,7 @@
     settings = {
       env.TERM = "xterm-256color";
       font = {
-        size = 12;
+        size = 9;
       };
       scrolling.multiplier = 5;
       selection.save_to_clipboard = true;
