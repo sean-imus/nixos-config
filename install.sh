663891bfe519fd561723ad0f8102b82534bbcefb
Sean Tietz <sean.tietz2@gmail.com>
2026-04-16 09:02:59 +0200

    fix perms and echo statements

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-echo "Backing up old configuration"
+echo "Backing up current configuration"
 sudo mv /etc/nixos /etc/nixos.bak
 
-echo "Symlinking new configuration to configuration directory"
+echo "Symlinking configuration to /etc/nixos"
 sudo ln -s ~/nixos-config /etc/nixos
 
-echo "Overriding current hardware configuration with new hardware information"
+echo "Copying hardware configuration from backup"
 sudo cp -f /etc/nixos.bak/hardware-configuration.nix ~/nixos-config/
 
-echo "Generating SSH Keys for github access"
+echo "Generating SSH key for GitHub access"
 ssh-keygen -t ed25519 -C "sean.tietz2@gmail.com"
 
-echo "Setting github remote to be able to push changes to config repo"
+echo "Setting GitHub remote for pushing changes"
 git -C ~/nixos-config remote set-url origin git@github.com:sean-imus/nixos-config.git
 
-echo "Printing public key for setting up ssh access to config repo"
+echo "Displaying public SSH key for GitHub setup"
 cat ~/.ssh/id_ed25519.pub
 
-echo "Press \"yes\" here to add github to your known host list to avoid future errors, it will fail if you havent added your public key to github but it will still add it to your known hosts list"
+echo "Adding GitHub to known hosts (you may see a key mismatch error if the key isn't added to GitHub yet, this is normal)"
 ssh -T git@github.com
 
-echo "Rebuilding NixOS configuration which will be applied on next boot. WARNING: This takes some time and shouldnt be interrupted"
+echo "Rebuilding NixOS configuration which will be applied on next boot. WARNING: This takes some time and shouldn't be interrupted"
 sudo nixos-rebuild boot
 
-echo "Finished! Reboot to use your configuration or explore this base gnome system further"
+echo "Finished! Reboot to use your configuration or continue exploring the base GNOME system"
