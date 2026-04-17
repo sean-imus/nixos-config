735bfe610f11ad1ef152a3e09adcadc4108e93a1
Sean Tietz <sean.tietz2@gmail.com>
2026-04-17 07:30:52 +0200

    attempt virtualbox setup

--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ./dotfiles/virtualbox.nix
   ];
 
   # Bootloader
---
+++ dotfiles/virtualbox.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }:
+
+{
+  # Install Virtualbox
+  virtualisation.virtualbox.host.enable = true;
+
+  # Add users to virtualbox group to allow usage
+  users.users.sean.extraGroups = [ "vboxusers" ];
+}
