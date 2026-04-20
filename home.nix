46a8600ec4649f69cf4742016b523a7ee1cf2836
Sean Tietz <sean.tietz2@gmail.com>
2026-04-20 11:46:52 +0200

    fix

--- home.nix
+++ home.nix
@@ -2,15 +2,15 @@
 
 {
   imports = [
-    ../dotfiles/btop.nix
-    ../dotfiles/chromium.nix
-    ../dotfiles/git.nix
-    ../dotfiles/ssh.nix
-    ../dotfiles/alacritty.nix
-    ../dotfiles/neovim.nix
-    ../dotfiles/opencode.nix
-    ../dotfiles/network-tools.nix
-    ../dotfiles/remmina.nix
+    ./dotfiles/btop.nix
+    ./dotfiles/chromium.nix
+    ./dotfiles/git.nix
+    ./dotfiles/ssh.nix
+    ./dotfiles/alacritty.nix
+    ./dotfiles/neovim.nix
+    ./dotfiles/opencode.nix
+    ./dotfiles/network-tools.nix
+    ./dotfiles/remmina.nix
   ];
 
   home.username = "sean";
