bf2dc96aacbb8d182cf0bc62cb8ad6571fd466a0
Sean Tietz <sean.tietz2@gmail.com>
2026-04-21 09:41:23 +0200

    try first configuration

--- dotfiles/hyprland.nix
+++ dotfiles/hyprland.nix
@@ -3,5 +3,9 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
+    extraConfig = ''
+      bindd = SHIFT, RETURN, Terminal, exec, alacritty
+      bindd = SHIFT, B, Browser, exec, chromium
+    '';
   };
 }
