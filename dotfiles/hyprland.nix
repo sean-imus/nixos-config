82a4bd19541cdceefb150e9b6162e35913cd2f57
Sean Tietz <sean.tietz2@gmail.com>
2026-04-21 10:02:15 +0200

    fix

--- dotfiles/hyprland.nix
+++ dotfiles/hyprland.nix
@@ -4,10 +4,15 @@
   wayland.windowManager.hyprland = {
     enable = true;
     extraConfig = ''
-      $mainMod = RIGHTALT
+      $mainMod = SUPER
 
+      # Applications
       bind = $mainMod, RETURN, exec, alacritty
       bind = $mainMod SHIFT, B, exec, chromium
+
+      # Functionality
+      bind = $mainMod, W, killactive,
+      bind = $mainMod, M, exit,
     '';
   };
 }
