5c5bdf8aca92964d4a0c7ce4d59daa80a9bcc40d
Sean Tietz <sean.tietz2@gmail.com>
2026-04-24 21:37:56 +0200

    feat(firefox): Added declarative Bookmarks

--- features/firefox.nix
+++ features/firefox.nix
@@ -16,6 +16,19 @@
             ublock-origin
           ];
         };
+	bookmarks = {
+	  force = true;
+	  settings = [
+	    {
+	      name = "NixOS";
+	      toolbar = true;
+	      bookmarks = [
+	        { name = "NixOS Search"; url = "https://search.nixos.org/packages"; }
+	        { name = "Home-Manager Search"; url = "https://home-manager-options.extranix.com/?query=&release=master"; }
+	      ];
+	    }
+	  ];
+	};
       };
     };
   };
