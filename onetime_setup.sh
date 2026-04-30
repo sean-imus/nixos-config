a5e55294406dcfb97fc36c325292851dbdb8bd4f
Sean Tietz <sean.tietz2@gmail.com>
2026-04-30 08:14:45 +0200

    formatting

--- onetime_setup.sh
+++ onetime_setup.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-echo "Backing up current configuration"
+echo "Backing up Current Configuration"
 sudo mv /etc/nixos /etc/nixos.bak
 
-echo "Symlinking configuration to /etc/nixos"
+echo "Symlinking Configuration to /etc/nixos"
 sudo ln -s ~/nixos-config /etc/nixos
 
-echo "Generating SSH key for GitHub access"
+echo "Generating SSH key for GitHub Access"
 ssh-keygen -t ed25519 -C "sean.tietz2@gmail.com" -N "" -f ~/.ssh/id_ed25519 -q
 
-echo "Setting GitHub remote for pushing changes"
+echo "Setting GitHub Remote for Pushing Changes"
 git -C ~/nixos-config remote set-url origin git@github.com:sean-imus/nixos-config.git
 
-echo "Displaying public SSH key for GitHub setup"
+echo "Displaying Public SSH Key for GitHub Setup"
 cat ~/.ssh/id_ed25519.pub
 
-echo "Adding GitHub to known hosts"
+echo "Adding GitHub to Known Hosts"
 ssh-keyscan github.com >>~/.ssh/known_hosts
