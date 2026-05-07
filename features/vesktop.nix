a75a41affffa384df6ed2eab3f4d6516c8db03fd
Sean Tietz <sean.tietz2@gmail.com>
2026-05-07 08:51:46 +0200

    feat: enhance vesktop and vscode configurations with RPC support

--- features/vesktop.nix
+++ features/vesktop.nix
@@ -6,6 +6,19 @@
   homeManagerModule = {
     programs.vesktop = {
       enable = true;
+      settings = {
+        discordBranch = "stable";
+        tray = false;
+        autoStartMinimized = false;
+        arRPC = true;
+        firstLaunch = false;
+        linuxAutoStartEnabled = false;
+        checkUpdates = false;
+        hardwareAcceleration = true;
+      };
+      vencord.settings = {
+        autoUpdate = false;
+      };
     };
   };
 }
