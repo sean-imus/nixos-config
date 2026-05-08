685475450625ce6f0b904b1602786220575ae66e
sean tietz <sean.tietz2@gmail.com>
2026-05-08 14:43:51 +0200

    feat: add settings for Mako notification daemon configuration

--- features/niri/niri.nix
+++ features/niri/niri.nix
@@ -29,6 +29,21 @@ in
     # Notification Daemon
     services.mako = {
       enable = true;
+      settings = {
+        anchor = "top-right";
+        background-color = "#00000088";
+        text-color = "#ffffff";
+        border-color = "#437306";
+        border-radius = 12;
+        border-size = 2;
+        default-timeout = 5000;
+        font = "Sans 11";
+        height = 100;
+        width = 400;
+        margin = "15";
+        padding = "5,15";
+        max-visible = 5;
+      };
     };
 
     # Keyboard Audio Button Daemon
