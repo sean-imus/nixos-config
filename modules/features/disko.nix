8bd0fd605c592f2c2a0115213a46e1d7b4bafd87
sean tietz <sean.tietz2@gmail.com>
2026-05-13 23:24:50 +0200

    add disko feature with overridable root device for other hosts

--- modules/features/disko.nix
+++ modules/features/disko.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ inputs, lib, ... }:
 {
   flake-file.inputs = {
     disko = {
@@ -6,4 +6,64 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  flake.modules.nixos.disko =
+    { config, ... }:
+    {
+      imports = [ inputs.disko.nixosModules.disko ];
+
+      options.diskoConfigDevice = lib.mkOption {
+        type = lib.types.str;
+        default = "/dev/sda";
+        description = "Root disk device, prefer /dev/disk/by-id/";
+      };
+
+      config.disko.devices = {
+        disk.main = {
+          type = "disk";
+          device = config.diskoConfigDevice;
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                size = "512M";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "umask=0077" ];
+                };
+              };
+              luks = {
+                size = "100%";
+                content = {
+                  type = "luks";
+                  name = "cryptroot";
+                  settings.allowDiscards = true;
+                  content = {
+                    type = "btrfs";
+                    extraArgs = [ "-f" ];
+                    subvolumes = {
+                      "/nix" = {
+                        mountpoint = "/nix";
+                        mountOptions = [ "compress=zstd" "noatime" ];
+                      };
+                      "/persist" = {
+                        mountpoint = "/persist";
+                        mountOptions = [ "compress=zstd" "noatime" ];
+                      };
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+        nodev."/" = {
+          fsType = "tmpfs";
+          mountOptions = [ "size=8G" ];
+        };
+      };
+    };
 }
