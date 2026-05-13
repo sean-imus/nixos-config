eb42d5588176a8935f675f21a1ee5c93ce84b5f0
sean tietz <sean.tietz2@gmail.com>
2026-05-14 00:06:54 +0200

    update script for new impermanence setup

--- onetime_setup.sh
+++ onetime_setup.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Backing up Current Configuration"
-sudo mv /etc/nixos /etc/nixos.bak
-
-echo "Symlinking Configuration to /etc/nixos"
-sudo ln -s ~/nixos-config /etc/nixos
+echo "Cloning Github Repo"
+git pull https://github.com/sean-imus/nixos-config.git ~/persist/nixos-config
 
 echo "Generating SSH key for GitHub Access"
 ssh-keygen -t ed25519 -C "sean.tietz2@gmail.com" -N "" -f ~/.ssh/id_ed25519 -q
 
 echo "Setting GitHub Remote for Pushing Changes"
-git -C ~/nixos-config remote set-url origin git@github.com:sean-imus/nixos-config.git
+git -C ~/persist/nixos-config remote set-url origin git@github.com:sean-imus/nixos-config.git
 
 echo "Displaying Public SSH Key for GitHub Setup"
 cat ~/.ssh/id_ed25519.pub
