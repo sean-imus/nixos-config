6afef7d3412281dedab5dc0504cc54b053d58a64
sean tietz <sean.tietz2@gmail.com>
2026-05-15 15:03:02 +0200

    added spellchecking declarative and more config keys set

--- modules/features/firefox.nix
+++ modules/features/firefox.nix
@@ -15,8 +15,10 @@
         configPath = "${config.xdg.configHome}/mozilla/firefox";
         profiles.${config.home.username} = {
           settings = {
+	    "app.normandy.first_run" = false;
             "extensions.autoDisableScopes" = 0;
             "browser.startup.homepage" = "about:newtab";
+	    "browser.aboutConfig.showWarning" = false;
             "browser.bookmarks.addedImportButton" = false;
             "browser.shell.checkDefaultBrowser" = false;
             "browser.toolbars.bookmarks.visibility" = "always";
@@ -24,15 +26,18 @@
             "browser.newtabpage.activity-stream.feeds.topsites" = false;
             "browser.newtabpage.activity-stream.showSponsoredCheckboxes" = false;
             "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+	    "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
             "sidebar.visibility" = "hide-sidebar";
             "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
             "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
+            "spellchecker.dictionary" = "en-US,de-DE";
           };
           extensions = {
             force = true;
             packages = [
               inputs.nix-firefox-addons.addons.${pkgs.stdenv.hostPlatform.system}.ublock-origin
               inputs.nix-firefox-addons.addons.${pkgs.stdenv.hostPlatform.system}.vimium-ff
+              inputs.nix-firefox-addons.addons.${pkgs.stdenv.hostPlatform.system}.dictionary-german
             ];
           };
         };
