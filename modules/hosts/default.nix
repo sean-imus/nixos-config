eb31141f27eb14db8e9f17fc5215d5fa8bd23942
sean tietz <sean.tietz2@gmail.com>
2026-05-28 00:04:21 +0200

    move zsh installing here

--- modules/hosts/default.nix
+++ modules/hosts/default.nix
@@ -88,6 +88,8 @@
           system.stateVersion = "25.11";
           nixpkgs.hostPlatform = "x86_64-linux";
 
+          programs.zsh.enable = true;
+
           networking = {
             networkmanager.enable = true;
             firewall.enable = true;
