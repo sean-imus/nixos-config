1bbeed253010c917f7869d9e43029f72be080def
sean tietz <sean.tietz2@gmail.com>
2026-05-27 18:20:13 +0200

    dont need allat

--- modules/features/desktop/lockscreen.nix
+++ modules/features/desktop/lockscreen.nix
@@ -8,17 +8,7 @@
         settings = {
           ignore-empty-password = true;
           show-failed-attempts = true;
-          font = "Sans";
-          ring-color = "84c906";
-          inside-color = "00000088";
-          key-hl-color = "84c906";
-          bs-hl-color = "ff0000";
-          separator-color = "00000000";
-          line-color = "00000000";
-          text-color = "ffffff";
-          text-caps-lock-color = "ffffff";
-          indicator-radius = 100;
-          indicator-thickness = 10;
+          color = "000000";
         };
       };
 
