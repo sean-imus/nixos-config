913cc9d5c0da06f89257898366dde49f2f94314f
sean tietz <sean.tietz2@gmail.com>
2026-05-27 18:20:09 +0200

    all hosts need this when localsend is enabled

--- modules/features/desktop/filesharing.nix
+++ modules/features/desktop/filesharing.nix
@@ -1,14 +1,10 @@
-{ lib, ... }:
+{ ... }:
 {
   flake.modules.nixos.filesharing =
-    { config, ... }:
+    { ... }:
     {
-      options.hostCfg.filesharing.enable = lib.mkEnableOption "LocalSend firewall ports";
-
-      config = lib.mkIf config.hostCfg.filesharing.enable {
-        networking.firewall.allowedTCPPorts = [ 53317 ];
-        networking.firewall.allowedUDPPorts = [ 53317 ];
-      };
+      networking.firewall.allowedTCPPorts = [ 53317 ];
+      networking.firewall.allowedUDPPorts = [ 53317 ];
     };
 
   flake.modules.homeManager.filesharing =
