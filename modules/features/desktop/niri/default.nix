9c15574bf4c39c0217b2144cd8880bd6013abf8c
sean tietz <sean.tietz2@gmail.com>
2026-06-01 20:38:55 +0200

    remove niri animations for snappier computing

--- modules/features/desktop/niri/default.nix
+++ modules/features/desktop/niri/default.nix
@@ -35,38 +35,6 @@
 
   flake.modules.homeManager.niri =
     { ... }:
-    let
-      shaders.window-open = ''
-        vec4 expanding_circle(vec3 coords_geo, vec3 size_geo) {
-            vec3 coords_tex = niri_geo_to_tex * coords_geo;
-            vec4 color = texture2D(niri_tex, coords_tex.st);
-            vec2 coords = (coords_geo.xy - vec2(0.5, 0.5)) * size_geo.xy * 2.0;
-            coords = coords / length(size_geo.xy);
-            float p = niri_clamped_progress;
-            if (p * p <= dot(coords, coords))
-                color = vec4(0.0);
-            return color;
-        }
-        vec4 open_color(vec3 coords_geo, vec3 size_geo) {
-            return expanding_circle(coords_geo, size_geo);
-        }
-      '';
-      shaders.window-close = ''
-        vec4 closing_circle(vec3 coords_geo, vec3 size_geo) {
-            vec3 coords_tex = niri_geo_to_tex * coords_geo;
-            vec4 color = texture2D(niri_tex, coords_tex.st);
-            vec2 coords = (coords_geo.xy - vec2(0.5, 0.5)) * size_geo.xy * 2.0;
-            coords = coords / length(size_geo.xy);
-            float p = 1.0 - niri_clamped_progress;
-            if (p * p <= dot(coords, coords))
-                color = vec4(0.0);
-            return color;
-        }
-        vec4 close_color(vec3 coords_geo, vec3 size_geo) {
-            return closing_circle(coords_geo, size_geo);
-        }
-      '';
-    in
     {
       imports = [
         ./_keybindings.nix
@@ -203,22 +171,7 @@
 
           clipboard.disable-primary = true;
 
-          animations = {
-            window-open = {
-              kind.easing = {
-                duration-ms = 250;
-                curve = "linear";
-              };
-              custom-shader = shaders.window-open;
-            };
-            window-close = {
-              kind.easing = {
-                duration-ms = 250;
-                curve = "linear";
-              };
-              custom-shader = shaders.window-close;
-            };
-          };
+          animations.enable = false;
 
           window-rules = [
             {
