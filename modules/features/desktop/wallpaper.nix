5748b0c3768702119e6f5afe504300795dd38bfd
sean tietz <sean.tietz2@gmail.com>
2026-07-15 09:59:40 +0200

    add animated wallpaper

--- modules/features/desktop/wallpaper.nix
+++ modules/features/desktop/wallpaper.nix
@@ -2,15 +2,21 @@
 {
   flake.modules.homeManager.desktop =
     { pkgs, ... }:
+    let
+      wallpaper = toString (./. + "/../../../assets/landscape.mp4");
+    in
     {
-      home.packages = [ pkgs.swaybg ];
+      home.packages = [ pkgs.mpvpaper ];
 
       programs.niri.settings.spawn-at-startup = [
         {
           argv = [
-            "swaybg"
-            "-c"
-            "#1a2420"
+            "mpvpaper"
+            "-f"
+            "-o"
+            "no-audio --loop"
+            "ALL"
+            wallpaper
           ];
         }
       ];
