955dd1b32d7ff99984715fd910cce9f41dc56053
sean tietz <sean.tietz2@gmail.com>
2026-08-09 14:54:21 +0200

    no auto formatting

--- modules/features/dev/neovim.nix
+++ modules/features/dev/neovim.nix
@@ -86,23 +86,6 @@
             };
           };
 
-          conform-nvim = {
-            enable = true;
-            settings = {
-              format_on_save = {
-                lsp_format = "fallback";
-                timeout_ms = 500;
-              };
-              formatters_by_ft = {
-                nix = [ "nixfmt" ];
-                python = [
-                  "isort"
-                  "black"
-                ];
-              };
-            };
-          };
-
           neo-tree = {
             enable = true;
             settings = {
