4a6497ff6ed799535e4c32e823d2fcb2bbfa28e6
sean tietz <sean.tietz2@gmail.com>
2026-08-10 11:50:59 +0200

    more building of gaming notebook

--- modules/hosts/gaming-notebook.nix
+++ modules/hosts/gaming-notebook.nix
@@ -1,9 +1,46 @@
 { inputs, ... }:
+	let
+		hostName = "gaming-notebook";
+	in
 {
 	flake.modules.nixos."gaming-notebook" =
-	{ ... }:
+	{ pkgs, ... }:
 	{
 		imports = with inputs.self.modules.nixos; [ hostDefault disko ];
+
+		# Disk configuration
+		diskoCfg = {
+			device = "!!PLACEHOLDER!!";
+			memorySize = 32;
+			encrypt = false;
+		};
+
+		# Enable PPD for battery life on the go
+		services.power-profiles-daemon.enable = true;
+
+		# CPU configuration
+		hardware = {
+			enableRedistributableFirmware = true;
+			cpu.intel.updateMicrocode = true;
+		};
+
+		# Graphics configuration
+		hardware.graphics = {
+			enable = true;
+			enable32Bit = true;
+			extraPackages = with pkgs; [ intel-media-driver ];
+		};
+		environment.variables.LIBVA_DRIVER_NAME = "iHD";
+		services.xserver.videoDrivers = [ "nvidia" ];
+		hardware.nvidia = {
+			modesetting.enable = true;
+			open = true;
+			dynamicBoost.enable = true;
+			powerManagement.enable = true;
+		};
+
+		# Enable bluetooth support
+		hardware.bluetooth.enable = true;
 
 		# Enable audio
 		hardware.alsa.enableBluetooth = true;
@@ -18,6 +55,9 @@
 		};
 
 		# Set hostname which is also used for the rbs/rbb aliases to determine what host to rebuild
-		networking.hostName = "gaming-notebook";
-	};
+		networking.hostName = hostName;
+		};
+
+	# Logic to actually build the NixOS system
+	flake.nixosConfigurations = inputs.self.lib.mkNixos "x86_64-linux" hostName;
 }
