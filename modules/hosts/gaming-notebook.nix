595798f49797ecdeff7d85e41d6b22ded005934d
sean tietz <sean.tietz2@gmail.com>
2026-08-10 10:23:02 +0200

    more gaming notebook building

--- modules/hosts/gaming-notebook.nix
+++ modules/hosts/gaming-notebook.nix
@@ -1,19 +1,23 @@
 { inputs, ... }:
 {
-	flake.modules.nixos."work-notebook" =
+	flake.modules.nixos."gaming-notebook" =
 	{ ... }:
 	{
 		imports = with inputs.self.modules.nixos; [ hostDefault disko ];
 
-		config = {
-			hardware.alsa.enableBluetooth = true;
-			security.rtkit.enable = true;
-			services.pipewire = {
-				alsa.enable = true;
-				alsa.support32Bit = true;
+		# Enable audio
+		hardware.alsa.enableBluetooth = true;
+		security.rtkit.enable = true;
+		services.pipewire = {
+			enable = true;
+			pulse.enable = true;
+			alsa = {
 				enable = true;
-				pulse.enable = true;
+				support32Bit = true;
 			};
 		};
+
+		# Set hostname which is also used for the rbs/rbb aliases to determine what host to rebuild
+		networking.hostName = "gaming-notebook";
 	};
 }
