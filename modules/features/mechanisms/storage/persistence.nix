3444972ecfce490510a14c83ea4903e86384eca6
sean tietz <sean.tietz2@gmail.com>
2026-08-10 13:56:55 +0200

    completed persistence module

--- modules/features/mechanisms/storage/persistence.nix
+++ modules/features/mechanisms/storage/persistence.nix
@@ -1,15 +1,14 @@
-{ ... }:
+{ inputs, ... }:
 {
 	# Use preservation to persist files & folders on an impermanent root
   flake-file.inputs = {
     preservation = {
       url = "github:nix-community/preservation";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
 	flake.modules.nixos.persistence =
-	{ lib, config, inputs, ... }:
+	{ lib, config, ... }:
 	{
 		# Import preservations NixOS module to use its options
 		imports = [ inputs.preservation.nixosModules.default ];
@@ -32,10 +31,32 @@
               default = [ ];
 						};
 					};
-					# Everyone gets a user owned peristed directory under ~/persist
+					# Everyone gets a user owned persisted directory under ~/persist
 					config.persist.directories = [ "persist" ];
 				}
 			)
 		];
+
+		# Actual logic that moves files we want persisted to the correct locations at boot
+		preservation = {
+			enable = true;
+			preserveAt."/persist" = {
+				directories = [
+					# Needed so anything timer based knows when it last ran (Nix-Store optimization / old NixOS generation cleanup)
+					"/var/lib/systemd/timers"
+				];
+				files = [
+					{
+						# This is needed so the host has an actual machine-id since we disabled systemd from committing it
+						file = "/etc/machine-id";
+						inInitrd = true;
+					}
+				];
+				# Logic that maps any persisted files / directories from Home-Manager modules to the preservation module for all users
+				users = lib.mapAttrs (_name: hm: {
+        	inherit (hm.persist) files directories;
+        }) config.home-manager.users;
+			};
+		};
 	};
 }
