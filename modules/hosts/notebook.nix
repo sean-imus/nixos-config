8697de4f55cadc667d6b0a089f068ed12ab30b50
sean tietz <sean.tietz2@gmail.com>
2026-08-11 14:21:59 +0200

    unused notification stubs

--- modules/hosts/notebook.nix
+++ modules/hosts/notebook.nix
@@ -62,8 +62,6 @@
       services.upower = {
         enable = true;
         usePercentageForPolicy = true;
-        percentageLow = 20;
-        percentageCritical = 10;
         percentageAction = 5;
         criticalPowerAction = "Hibernate";
       };
