ac33d0c09c1a1a5b03d88106c86b05493db16452
sean tietz <sean.tietz2@gmail.com>
2026-08-11 14:20:45 +0200

    update boot kernel modules

--- modules/hosts/gaming-notebook.nix
+++ modules/hosts/gaming-notebook.nix
@@ -61,7 +61,7 @@ in
       };
 
       # Boot options
-      boot.initrd.availableKernelModules = [ "nvme" ];
+      boot.initrd.availableKernelModules = [ "nvme" "thunderbolt" "xhci_pci" "usbhid" ];
 
       # Enable hibernation safety net if battery falls to critical levels
       services.upower = {
