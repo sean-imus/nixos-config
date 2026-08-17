67e3f6a3fc1aaa14dc49efcd5c30a07c28e6aae6
sean tietz <sean.tietz2@gmail.com>
2026-08-17 10:26:17 +0200

    add office pack

---
+++ modules/features/desktop/office.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+{
+  home.packages = [ pkgs.libreoffice-fresh ];
+
+  xdg.dataFile = builtins.listToAttrs (
+    map
+      (name: {
+        name = "applications/${name}.desktop";
+        value.text = "[Desktop Entry]\nHidden=true\n";
+      })
+      [
+        "base"
+        "draw"
+        "impress"
+        "math"
+        "startcenter"
+        "xsltfilter"
+      ]
+  );
+}
