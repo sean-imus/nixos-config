177045f2b98deaa638f1a9b27941c5cd515321d5
sean tietz <sean.tietz2@gmail.com>
2026-08-17 10:23:43 +0200

    add rdp work thing

---
+++ modules/features/desktop/rdp-work.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+{
+  networking.networkmanager.ensureProfiles.profiles = {
+    "rdp-static-eth" = {
+      connection = {
+        id = "rdp-static-eth";
+        type = "ethernet";
+        interface-name = "enp44s0";
+      };
+      ipv4 = {
+        address = "192.168.200.2/24";
+        method = "manual";
+        "route-metric" = 100;
+      };
+      ipv6 = {
+        method = "ignore";
+      };
+    };
+  };
+
+  home-manager.sharedModules = [
+    {
+      home.packages = [ pkgs.freerdp ];
+
+      xdg.desktopEntries.rdp-to-work = {
+        name = "Connect to Work Laptop";
+        exec = "xfreerdp /v:192.168.200.1 /u:stietz /p: /d:ENTEX /f /dynamic-resolution /kbd:layout:0x0407,lang:0x0407";
+        terminal = false;
+        icon = ../../../assets/windows_logo.png;
+      };
+    }
+  ];
+}
