afe2019e02144464f405c21040f4d343e75746c5
sean tietz <sean.tietz2@gmail.com>
2026-08-17 14:26:49 +0200

    install sops and set location env

--- modules/features/secrets/sops.nix
+++ modules/features/secrets/sops.nix
@@ -1,5 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  home.packages = [ pkgs.sops ];
+
+  home.sessionVariables.SOPS_AGE_KEY_FILE = "/home/sean/.sops/age.key";
+
   sops = {
     defaultSopsFile = ./secrets.yaml;
     age.keyFile = "/home/sean/.sops/age.key";
