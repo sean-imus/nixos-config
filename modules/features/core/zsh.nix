2a79495b846ff378297871790ecf2fda5dd67599
sean tietz <sean.tietz2@gmail.com>
2026-08-17 09:36:07 +0200

    add zsh config

---
+++ modules/features/core/zsh.nix
@@ -0,0 +1,55 @@
+{ ... }:
+{
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    enableCompletion = true;
+    history = {
+      size = 10000;
+      save = 10000;
+      share = true;
+      ignoreAllDups = true;
+      extended = true;
+    };
+    initContent = ''
+      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+    '';
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      add_newline = false;
+      format = "$cmd_duration$directory$git_branch$git_status$character";
+
+      cmd_duration = {
+        min_time = 2000;
+        format = "[($duration)]($style) ";
+        style = "bold #dbbc7f";
+      };
+
+      directory = {
+        format = "([$path]($style) )";
+        style = "bold #83c092";
+        truncate_to_repo = false;
+      };
+
+      git_branch = {
+        format = "[$branch ]($style)";
+        style = "bold #d699b6";
+      };
+
+      git_status = {
+        format = "([$all_status$ahead_behind]($style) )";
+        style = "bold #e67e80";
+      };
+
+      character = {
+        success_symbol = "[❯](bold #a7c080)";
+        error_symbol = "[❯](bold #e67e80)";
+      };
+    };
+  };
+}
