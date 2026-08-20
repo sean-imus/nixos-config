059b641073a3c4484f9f335963a9b636091364f0
sean tietz <sean.tietz2@gmail.com>
2026-08-20 13:08:43 +0200

    install two games

--- modules/features/gaming.nix
+++ modules/features/gaming.nix
@@ -1,5 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  home-manager.sharedModules = [
+    {
+      home.packages = with pkgs; [
+        the-powder-toy
+        ddnet
+      ];
+    }
+  ];
+
   programs.steam = {
     enable = true;
   };
