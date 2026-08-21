ad8de3aed699e84b7bb5a28cbb6cb65ea42d4693
sean tietz <sean.tietz2@gmail.com>
2026-08-21 13:44:13 +0200

    fix(power): configure valid hibernation thresholds

--- modules/notebook.nix
+++ modules/notebook.nix
@@ -54,6 +54,8 @@
   services.upower = {
     enable = true;
     usePercentageForPolicy = true;
+    percentageLow = 20;
+    percentageCritical = 10;
     percentageAction = 5;
     criticalPowerAction = "Hibernate";
   };
